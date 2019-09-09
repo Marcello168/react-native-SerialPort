0795f6b65c9bfeb68d7d31cb0ca97ce7d8ed3ccd
gongyonghui <gongyonghui@gongyonghuideMac-mini.local>
2019-09-09 13:23:09 +0800

    no message

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
+/*
+ * @Author: gongyonghui
+ * @Date: 2019-09-09 09:35:05
+ * @LastEditors: gongyonghui
+ * @LastEditTime: 2019-09-09 13:22:02
+ * @Description: file content
+ */
 
-import { NativeModules } from 'react-native';
+import { NativeModules, DeviceEventEmitter } from 'react-native';
 
 const { RNSerialPort } = NativeModules;
 
